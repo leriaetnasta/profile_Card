54d292f0ce49fc548211089747a059586effc788
leriaetnasta <61632665+leriaetnasta@users.noreply.github.com>
2025-04-22 15:37:02 +0100

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,17 +6,33 @@ import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { Profile } from "@/components/profile/profile";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-import {Text, TextInput, View} from 'react-native';
-import {styles} from '../app.styles';
+import { Text, TextInput, View } from "react-native";
+import { styles } from "../app.styles";
 
 export default function HomeScreen() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView >
-        <Profile/>
-      
+      <SafeAreaView>
+        <Profile
+          firstName={"loubna"}
+          lastName={"Talha"}
+          img={
+            <Image
+              source={{ uri: "https://i.pravatar.cc/300" }}
+            />
+          }
+        >
+          <View
+            style={{ height: 30, width: 30, backgroundColor: "pink" }}
+          ></View>{" "}
+        </Profile>
+        <Profile firstName={"Sara"} lastName={"Yul"}>
+          {" "}
+        </Profile>
+        <Profile firstName={"Java"} lastName={"JEE"}>
+          {" "}
+        </Profile>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
-
--- components/profile/profile.tsx
+++ components/profile/profile.tsx
@@ -1,19 +1,32 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Alert, Image, Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./profile.style";
 import Entypo from "@expo/vector-icons/Entypo";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
-export function Profile() {
+import { useState } from "react";
+export function Profile({firstName, lastName, img}:any) {
   // fragment : <></>
+  let [age, setAge]=useState(1)
+  function greetings(){
+    return 'Welcome!'
+  }
+  function increase(){
+    return setAge(++age);
+  }
+  function hello(){
+    return Alert.alert(firstName)
+  }
+  
   return (
     <View style={styles.container}>
       <Image
         style={styles.avatar}
         source={{ uri: "https://i.pravatar.cc/300" }}
       />
-      <Text style={styles.name}>Loubna Talha</Text>
+      <Text style={styles.name} onPress={hello} >{firstName} {lastName} {greetings()}</Text>
+      <TouchableOpacity onPress={increase} >
       <Text style={styles.text}>
-        Hi! I'm a software engineer, let's get in touch
-      </Text>
+        Hi! I'm a software engineer, I'm {age}, let's get in touch
+      </Text></TouchableOpacity>
       <View style={styles.social}>
         <TouchableOpacity style= {styles.socialBtn}>
           {" "}
